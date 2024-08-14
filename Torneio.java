34bb78fcb1b110c2abae007e1c212f635375bbc6
Pedro Merisio <merisio.office@gmail.com>
2024-08-14 14:18:53 -0300

    atualizado

--- Torneio.java
+++ Torneio.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 // Classe Torneio, para a execucao das rodadas e manipulacao de vetores da classe Jogador.
 public class Torneio{
     Scanner teclado = new Scanner(System.in);
-    private Jogador[] jogadores = new Jogador[10]; // Vetor de 10 jogadores (maximo permitido).
+    private static Jogador[] jogadores = new Jogador[10]; // Vetor de 10 jogadores (maximo permitido).
     private int numJogadores; // Numero de jogadores nesse campeonato.
     private int numRodadas; // Numero de rodadas.
 
@@ -11,7 +11,6 @@ public class Torneio{
     public int getRodada(){
         return numRodadas;
     }
-
     public int getJogadores(){
         return numJogadores;
     }
@@ -20,27 +19,25 @@ public class Torneio{
     public void setRodada(int nr){
         this.numRodadas = nr;
     }
-
     public void setJogadores(int nj){
         this.numJogadores = nj;
     }
 
-    // Realiza a adicao de um jogador
-    public void addJogador(){
-        System.out.println("Insira o tipo do jogador (0 para humano e 1 para maquina):");
-        int tipo = teclado.nextInt();
-
-        jogadores[getJogadores] = new Jogador(tipo);
-
-        setJogadores(num + 1);
+    // Realiza a adicao dos jogadores
+    public void addJogadores(){
+        for (int i = 0; i < this.numJogadores; i++){
+            jogadores[i].setSaldo(100);
+            jogadores[i].setId(i);
+            System.out.println("Insira o tipo do jogador" + i + "(0 para humano e 1 para maquina):");
+            int tipo = teclado.nextInt();
+            jogadores[i].setTipo(tipo);
+        }
     }
-    
+
     // Inicia o torneio
     public void iniciarTorneio(int numJog){
         setJogadores(numJog);
-
-        for (int i = 0; i < numJogadores; i++)
-            jogadores[i].setSaldo(100);
+        addJogadores();
 
         System.out.println("1) Jogo de Azar;");
         System.out.println("2) Bozo;");
@@ -64,9 +61,8 @@ public class Torneio{
     }
 
     public void mostrarPlacarFinal(){
-
+        
     }
-
+}
 
     
-}
