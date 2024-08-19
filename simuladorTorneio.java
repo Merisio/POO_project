835074224980a0e467db5fa5c814a6f9592b4ee1
Reis-GUI <guisreis03@gmail.com>
2024-08-19 16:09:17 -0300

    agora vai 4.0

--- simuladorTorneio.java
+++ simuladorTorneio.java
@@ -12,38 +12,42 @@ public class simuladorTorneio implements Serializable{
     public static void main(String[] args){
         System.out.println("Insira o numero de jogadores: ");
         int num = teclado.nextInt();
+        int sair =0;
 
-        if (num >= 2){
-            Torneio campeonato = new Torneio();
-            System.out.println("==== MENU INICIAL ====");
-            System.out.println("1) Comecar novo Campeonato.");
-            System.out.println("2) Ver placar.");
-            System.out.println("3) Salvar Campeonato.");
-            System.out.println("4) Carregar Campeonato.");
-            System.out.println("5) Sair.");
-            System.out.println("Selecione o que deseja:");
-            int opcao = teclado.nextInt();
+        while(sair != 5){
+            if (num >= 2){
+                Torneio campeonato = new Torneio();
+                System.out.println("==== MENU INICIAL ====");
+                System.out.println("1) Comecar novo Campeonato.");
+                System.out.println("2) Ver placar.");
+                System.out.println("3) Salvar Campeonato.");
+                System.out.println("4) Carregar Campeonato.");
+                System.out.println("5) Sair.");
+                System.out.println("Selecione o que deseja:");
+                int opcao = teclado.nextInt();
 
-            switch (opcao) {
-                case 1:
-                    campeonato.iniciarTorneio(num);  
-                    break;
-                case 2:
-                    campeonato.mostrarPlacarFinal();
-                    break;
-                case 3:
-                    gravar_arquivo();
-                    break;
-                case 4: 
-                    ler_arquivo();
-                    break;
-                case 5:
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("Opcao Invalida.");
-                    break;
-            }
+                switch (opcao) {
+                    case 1:
+                        campeonato.iniciarTorneio(num);  
+                        break;
+                    case 2:
+                        campeonato.mostrarPlacarFinal();
+                        break;
+                    case 3:
+                        gravar_arquivo();
+                        break;
+                    case 4: 
+                        ler_arquivo();
+                        break;
+                    case 5:
+                        System.out.println("Saindo...");
+                        sair =5;
+                        break;
+                    default:
+                        System.out.println("Opcao Invalida.");
+                        break;
+                }
+        }
         }
     }
 
